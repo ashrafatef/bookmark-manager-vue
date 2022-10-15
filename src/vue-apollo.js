24b4be614382299e19e6cea64b24c66c6008dd7a
ashrafatef <ashraaf.ateef@gmail.com>
2022-10-15 23:59:05 +0200

    remove ws

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -17,39 +17,39 @@ export const filesRoot = process.env.VUE_APP_FILES_ROOT || httpEndpoint.substr(0
 Vue.prototype.$filesRoot = filesRoot
 
 // Config
-const defaultOptions = {
-  // You can use `https` for secure connection (recommended in production)
-  httpEndpoint,
-  // You can use `wss` for secure connection (recommended in production)
-  // Use `null` to disable subscriptions
-  wsEndpoint: null,
-  // LocalStorage token
-  tokenName: AUTH_TOKEN,
-  // Enable Automatic Query persisting with Apollo Engine
-  persisting: false,
-  // Use websockets for everything (no HTTP)
-  // You need to pass a `wsEndpoint` for this to work
-  websocketsOnly: false,
-  // Is being rendered on the server?
-  ssr: false,
+// const defaultOptions = {
+//   // You can use `https` for secure connection (recommended in production)
+//   httpEndpoint,
+//   // You can use `wss` for secure connection (recommended in production)
+//   // Use `null` to disable subscriptions
+//   wsEndpoint: null,
+//   // LocalStorage token
+//   tokenName: AUTH_TOKEN,
+//   // Enable Automatic Query persisting with Apollo Engine
+//   persisting: false,
+//   // Use websockets for everything (no HTTP)
+//   // You need to pass a `wsEndpoint` for this to work
+//   websocketsOnly: false,
+//   // Is being rendered on the server?
+//   ssr: false,
 
-  // Override default apollo link
-  // note: don't override httpLink here, specify httpLink options in the
-  // httpLinkOptions property of defaultOptions.
-  // link: myLink
+//   // Override default apollo link
+//   // note: don't override httpLink here, specify httpLink options in the
+//   // httpLinkOptions property of defaultOptions.
+//   // link: myLink
 
-  // Override default cache
-  // cache: myCache
+//   // Override default cache
+//   // cache: myCache
 
-  // Override the way the Authorization header is set
-  // getAuth: (tokenName) => ...
+//   // Override the way the Authorization header is set
+//   // getAuth: (tokenName) => ...
 
-  // Additional ApolloClient options
-  // apollo: { ... }
+//   // Additional ApolloClient options
+//   // apollo: { ... }
 
-  // Client local data (see apollo-link-state)
-  // clientState: { resolvers: { ... }, defaults: { ... } }
-}
+//   // Client local data (see apollo-link-state)
+//   // clientState: { resolvers: { ... }, defaults: { ... } }
+// }
 
 
 const authLink = setContext((_, { headers }) => {
