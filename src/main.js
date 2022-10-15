d55c989693ba6ea6b23bcbae0a305c4110f7b83c
ashrafatef <ashraaf.ateef@gmail.com>
2022-10-15 23:40:27 +0200

    disbale apollo

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
-import { createProvider } from './vue-apollo'
+// import { createProvider } from './vue-apollo'
 import store from './store'
 
 Vue.config.productionTip = false
@@ -11,7 +11,7 @@ Vue.config.productionTip = false
 new Vue({
   vuetify,
   router,
-  apolloProvider: createProvider(),
+  // apolloProvider: createProvider(),
   store,
   render: h => h(App)
 }).$mount('#app')
